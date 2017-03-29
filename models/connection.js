11c0ee3cdc35ed93a7ff7048f9d8a9f3b7cc5075
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-03-29 11:56:05 +0530

    changes

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost'+'/fpc_db' , function(err) {
 	if(err) {
-		if (err) throw err;
+		throw err;
 	}
 	console.log("Connected to database ") ; 
 });
